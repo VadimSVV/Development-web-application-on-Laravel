a8438cf635362fc39c952c74441caeac0a7c4380
VadimSVV <korsarishe@gmail.com>
2023-03-22 19:01:14 +0300

    templates

---
+++ templates/index.php
@@ -0,0 +1,13 @@
+require_once ("templates/top.php");
+    if (!_GET['id']{
+        $file = (int)$_GET['id'];
+    }else{
+        $file = "index";
+    }
+    $query = "SELECT * FROM news WHERE id='".$file."' AND hide='show'";
+        $adr = mysql_query($query);
+    if (!$adr) exit($query);
+    $tbl_users = mysql_fetch_array($adr);
+    echo $tbl_users['name'];
+    echo $tbl_users['body'];
+require_once ("templates/bottom.php");    
